fa1fc44cc1b15fa48f8885b763064e5a109a3b90
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-05-25 21:47:20 -0300

    ✨ adiciona função para criar cards de sugestões (sem detalhes e destaque), e uma função simples para preencher sugestão selecionada no input

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,43 +1,82 @@
 
 // AUTOCOMPLETE / CAMPO DE BUSCA / TAGS
+const API_KEY = '459371bbb4e14c7e916851c51e8be668';
+
+async function fetchData(valueReq, APIKEY){
+   try {
+      const response = await fetch(`https://api.spoonacular.com/food/ingredients/search?query=${valueReq}&apiKey=${APIKEY}`);
+      const data = await response.json();
+      // console.log(data.result);
+      return data.results;
+   } catch (error) {
+      console.log(`fetchData function error ${error}`);
+   }
+}
+
+async function fetchRecipes(value, APIKEY) {
+   try {
+      const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${value}&apiKey=${APIKEY}`);
+      const data = await response.json();
+      // console.log(data.result);
+      return data.results;
+   } catch (error) {
+      console.log(`fetchData function error ${error}`);
+   }
+}
 
 
 
-function autocompleteMatch(input) {
-   if(input === ''){
+async function showSuggestions(value){
+   if(value === ''){
       return [];
    }
 
-   let regex = new RegExp(input)
-   return searchTerms.filter((term) => {
-      if(term.match(reg)){
-         return term;
+   const ingredients = await fetchData(value, API_KEY);
+   console.log(ingredients);
+
+
+   const list = document.querySelector("#selected-ingredients");
+   let limit = 0;
+   list.innerHTML = '';
+   ingredients.map((ingredient) => {
+      if(limit < 10){
+         let element = document.createElement('li');
+         element.classList.add("suggestionItem");
+         element.textContent = ingredient.name;
+         list.append(element);
+         limit++
       }
    })
+
 }
 
-function showResults(val) {
-   res = document.querySelector('#selected-ingredients');
+// TODO - REFINAR CÓDIGO DA FUNÇÃO ABAIXO QUE ATUALIZA INPUT COM SUGESTÃO CLICADA E ACIMA QUE CRIA SUGESTÕES (APRENDER A DESTACAR A SUGESTÃO CONFORME DIGITA)
 
-   if (val == '') {
-      return;
+function updateSearch(){
+   const items = document.querySelectorAll('.suggestionItem');
+   const input = document.querySelector('#ingredient-input');
+
+   if(items.length === 0){
+      return [];
    }
-   
-   fetch(`https://api.spoonacular.com/food/products/search?query=${val}&apiKey=${API_KEY}&number=20`)
-      .then(
-         function (response) {
-            return response.json();
-         }).then((data) => {
-            console.log(data)
-         }).catch((err) => {
-            console.warn('Something went wrong.', err);
-            return false;
-         });
-}
 
+   items.forEach((item) => {
+      item.addEventListener('click', () => {
+         input.value = item.textContent;
+         // console.log(item.value)
+      })
+   })
+   console.log(items)
+}
 
 const input = document.querySelector('#ingredient-input');
-input.addEventListener('change', (e) => showResults(e.target.value))
+input.addEventListener('keyup', (e) => {
+   showSuggestions(e.target.value);
+   // console.log(e.target.value);
+   setTimeout(() => {
+      updateSearch()
+   }, 1000)
+})
 // AUTOCOMPLETE / CAMPO DE BUSCA / TAGS
 
 
